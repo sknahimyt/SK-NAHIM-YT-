a1deb63e22daa1e255ee417581a113d1e2a4bf46
sknahimyt <sknahimyt678323@gmail.com>
2025-07-06 14:34:10 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-
-    const appUrl = 'SK NAHIM YT.apk';
-    
-   
-    const link = document.createElement('a');
-    link.href = appUrl;
-    link.download = 'SK NAHIM YT.apk';
-    
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    
-
-    alert('ডাউনলোড শুরু হচ্ছে...');
-});
+// Add animation or analytics later if needed
